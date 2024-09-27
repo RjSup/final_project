d5392e607ec15779bd77b29fc379b54a3c437dbb
RjSup <19141558@brookes.ac.uk>
2024-09-27 23:09:28 +0100

    EoD

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+# for communicating with algorithm import joblib
 
 app = Flask(__name__)
 CORS(app)
+
+# Load the machine learning model
+# model = joblib.load('model.joblib')
 
 @app.route('/', methods=['POST', 'GET'])
 def getSliderInfo():
@@ -14,14 +18,21 @@ def getSliderInfo():
         message = f"You chose {slider_value}"
         print(data)
         return jsonify({'message': message})
-    elif investment_amount is not None:
+        # send the data to the machine learning model
+    
+
+    elif investment_amount is not None and investment_amount > 0:
         message = f"Your investment amount is £{investment_amount}."
         print(data)
         return jsonify({'message': message})
+        # send the data to the machine learning model
+    
+
     else:
         message = "No valid data provided"
         print(data)
         return jsonify({'message': message})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
---
+++ backend/ml_algo.py
@@ -0,0 +1,10 @@
+# create model - model =
+
+# class of model
+
+# import dataset files ready to use algorithm
+
+
+
+# send model to backend - joblib.dump(model, 'model.joblib')
+
